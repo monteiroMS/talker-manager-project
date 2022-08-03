24908516756fa48fd0720dc5267deb8759b430aa
Matheus Monteiro <matheuschran@hotmail.com>
2022-08-03 19:40:45 +0000

    insere funcao de leitura de arquivos direto no talkerRouter

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const getTalkers = require('../middlewares/helpers/getTalkers');
+const fs = require('fs/promises');
 
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
-  const content = await getTalkers('talker.json');
-  return res
-    .status(200)
-    .json(content);
+  try {
+    const content = JSON.parse(await fs.readFile('talker.json', { encoding: 'utf8' }));
+    return res.status(200).json(content);
+  } catch (error) {
+    return res.status(200).json([]);
+  }
 });
 
 // router.get('/:id', async (_req, res) => {
